4b7a606ab7deaa0f55d494d971cfcd7e63cb731a
Korolev Denis87 <kordens-87@mail.ru>
2026-03-30 00:55:20 +0300

    правки 00:55

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: './js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/kata_project_2/'
+    publicPath: '/kata_project_2_maket/'
   },
 
   // Source maps для удобства отладки
